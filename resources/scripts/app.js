8f1d43609536fdd371a1968d8dc38ea6080da7fa
Ezequiel <ender.game.dev@gmail.com>
2024-06-12 20:57:26 -0300

    Improving the JS code

--- resources/scripts/app.js
+++ resources/scripts/app.js
@@ -28,10 +28,47 @@ const showingText = new IntersectionObserver((entries) => {
     });
 });
 
+const hexagonOneShows = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add("hexagon1-coming-down");
+        } else {
+            entry.target.classList.remove("hexagon1-coming-down");
+        }
+    });
+});
+
+const hexagonTwoShows = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add("hexagon2-coming-down");
+        } else {
+            entry.target.classList.remove("hexagon2-coming-down");
+        }
+    });
+});
+
+const hexagonThreeShows = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add("hexagon3-coming-up");
+        } else {
+            entry.target.classList.remove("hexagon3-coming-up");
+        }
+    });
+});
+
+function observeElement(observedElement, observer) {
+    observedElement.forEach((element) => observer.observe(element));
+}
+
 const hexagons = document.querySelectorAll(".hexagon-shape");
+const hexagonOne = document.querySelector(".hexagon-container1");
+const hexagonTwo = document.querySelector(".hexagon-container2");
+const hexagonThree = document.querySelector(".hexagon-container3");
 const icons = document.querySelectorAll(".icon");
 const text = document.querySelectorAll(".text-container");
 
-hexagons.forEach((element) => hexagonVisibility.observe(element));
-icons.forEach((element) => hidingIcons.observe(element));
-text.forEach((element) =>showingText.observe(element));
+observeElement(hexagons, hexagonVisibility);
+observeElement(icons, hidingIcons);
+observeElement(text, showingText);
